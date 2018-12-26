3a1c69af2782e5392bc7bb1dca09805b6aaee687
YixuanWei <yixuan_wei@qq.com>
2018-12-26 13:56:16 +0800

    errorMore

--- src/MiniJava/ErrorListener.java
+++ src/MiniJava/ErrorListener.java
@@ -1,9 +1,6 @@
 package MiniJava;
 
-import org.antlr.v4.runtime.BaseErrorListener;
-import org.antlr.v4.runtime.Parser;
-import org.antlr.v4.runtime.RecognitionException;
-import org.antlr.v4.runtime.Recognizer;
+import org.antlr.v4.runtime.*;
 
 import java.util.Collections;
 import java.util.List;
@@ -17,5 +14,23 @@ public class ErrorListener extends BaseErrorListener {
         Collections.reverse(stack);
         System.err.println("rule stack: "+stack);
         System.err.println("line "+line+":"+charPositionInLine+ " at "+offendingSymbol+": "+msg);
+        underlineError(recognizer,(Token)offendingSymbol,line,charPositionInLine);
+    }
+
+    protected void underlineError(Recognizer recognizer,Token offendingToken, int line, int charPositionInLine)
+    {
+        CommonTokenStream tokens=(CommonTokenStream)recognizer.getInputStream();
+        String input = tokens.getTokenSource().getInputStream().toString();
+        String[] lines = input.split("\n");
+        String errorLine = lines[line-1];
+        System.err.println(errorLine);
+        for(int i=0; i<charPositionInLine; i++) System.err.print(" ");
+        int start = offendingToken.getStartIndex();
+        int stop = offendingToken.getStopIndex();
+        if(start>=0 && stop>=0){
+            for(int i=start; i<=stop;i++) System.err.print("^");
+
+        }
+        System.err.println();
     }
 }
