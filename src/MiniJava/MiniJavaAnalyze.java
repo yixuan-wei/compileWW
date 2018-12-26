bc323b52625282d8351d9560aa398d0508616a93
YixuanWei <yixuan_wei@qq.com>
2018-12-26 10:29:12 +0800

    ASTInit

--- src/MiniJava/MiniJavaAnalyze.java
+++ src/MiniJava/MiniJavaAnalyze.java
@@ -19,7 +19,30 @@ public class MiniJavaAnalyze {
         MiniJavaLexer lexer = new MiniJavaLexer(input);
         CommonTokenStream tokens = new CommonTokenStream(lexer);
         MiniJavaParser parser = new MiniJavaParser(tokens);
+        ParserRuleContext ctx = parser.goal();
+        generateAST(ctx, false, 0);
+        //TODO utilize graphviz to print graph for AST
+    }
 
+    private static void generateAST(RuleContext ctx, boolean verbose, int indentation) {
+        boolean ignored = !verbose && ctx.getChildCount() == 1 && ctx.getChild(0) instanceof ParserRuleContext;
+
+        if (!ignored) {
+            String ruleName = MiniJavaParser.ruleNames[ctx.getRuleIndex()];
+            System.out.println("ruleName "+ruleName);
+            //Sequence.add(Integer.toString(indentation));
+            //Type.add(ruleName);
+            //Text.add(ctx.getText());
+            //TODO show with graphviz
+        }
+        for (int i = 0; i < ctx.getChildCount(); i++) {
+            ParseTree element = ctx.getChild(i);
+            //TODO switch among different context
+            if (element instanceof RuleContext) {
+                System.out.println(element.getText());
+                generateAST((RuleContext) element, verbose, indentation + (ignored ? 0 : 1));
+            }
+        }
     }
 
 }
