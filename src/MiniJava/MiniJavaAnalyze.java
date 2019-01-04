e583e1b41be2ff190b9f3cfac599a22253ea242e
WeiyueWang <15307130349@fudan.edu.cn>
2019-01-05 01:02:10 +0800

    TreeReviseTrue

--- src/MiniJava/MiniJavaAnalyze.java
+++ src/MiniJava/MiniJavaAnalyze.java
@@ -9,11 +9,6 @@ import java.io.*;
 import java.util.ArrayList;
 
 public class MiniJavaAnalyze {
-    static ArrayList<String> Parent = new ArrayList<String>();
-    static ArrayList<String> Sequence = new ArrayList<String>();
-    static ArrayList<String> Type = new ArrayList<String>();
-    static ArrayList<String> Text = new ArrayList<String>();
-    static ArrayList<String> SymbolCnt = new ArrayList<String>();
     static String[] rules;
 
     public static void main(String[] args) throws Exception{
@@ -34,7 +29,6 @@ public class MiniJavaAnalyze {
         parser.getInterpreter().setPredictionMode(PredictionMode.LL_EXACT_AMBIG_DETECTION);
         //generate AST
         ParserRuleContext ctx = parser.goal();
-        //generateAST(ctx, false, 0);
         //TODO utilize graphviz to print graph for AST
         PrintStream out;
         try{
@@ -42,7 +36,7 @@ public class MiniJavaAnalyze {
             System.setOut(out);
             System.out.println("digraph AST{");
             //toDot();
-            generateAST(ctx,false,"00","");
+            generateAST(ctx,true,"0","");
             System.out.println("}");
         } catch (FileNotFoundException e){
             e.printStackTrace();
@@ -55,20 +49,23 @@ public class MiniJavaAnalyze {
         Process p = Runtime.getRuntime().exec(command);
 
         InputStream is2 = p.getInputStream();
+        InputStream errorIS = p.getErrorStream();
         BufferedReader reader = new BufferedReader(new InputStreamReader(is2));
+        BufferedReader errReader = new BufferedReader(new InputStreamReader(errorIS));
         p.waitFor();
         if (p.exitValue() != 0) {
-            //TODO error handle
-            //说明命令执行失败
-            //可以进入到错误处理步骤中
+            System.out.println("ERROR: execute the generating graph.png order failed!");
         }
         p = Runtime.getRuntime().exec(openCmd);
         p.waitFor();
         if(p.exitValue()!=0){
-            //TODO error handle
+            System.out.println("ERROR: execute the open graph.png order failed!");
         }
         String s = null;
         while ((s = reader.readLine()) != null) {
+            System.out.println(s);
+        }
+        while((s=errReader.readLine())!=null){
             System.out.println(s);
         }
     }
@@ -77,62 +74,29 @@ public class MiniJavaAnalyze {
         boolean ignored = !verbose && ctx.getChildCount() == 1 && ctx.getChild(0) instanceof ParserRuleContext;
         if (!ignored) {
             String ruleName = MiniJavaParser.ruleNames[ctx.getRuleIndex()];
-            //System.out.println("ruleName "+ruleName);
-            System.out.println(indentation+"[label=\""+ruleName+"\n"+ctx.getText()+"\"]");
+            System.out.println(indentation+"[label=\""+ruleName+"\"]");
+            //System.out.println(indentation+"[label=\""+ruleName+"\n"+ctx.getText()+"\"]");
             if(!preIndentation.equals("")) {
-                //Parent.add(preIndentation);
                 System.out.println(preIndentation+"->"+indentation);
-                //Sequence.add(indentation);
-                //Type.add(ruleName);
-                //Text.add(ctx.getText());
             }
         }
-        int tempIndentation = Integer.valueOf(indentation);
         int tempChildCnt=0;
         for (int i = 0; i < ctx.getChildCount(); i++) {
             ParseTree element = ctx.getChild(i);
-            //System.out.println(element.getText());
             //TODO switch among different context
             if (element instanceof RuleContext) {
-                //System.out.println("is rule context");
-                //System.out.println(((RuleContext) element).getRuleIndex());
-                generateAST((RuleContext) element, verbose, String.valueOf(tempIndentation+10+tempChildCnt),indentation);
+                generateAST((RuleContext) element, verbose, indentation+tempChildCnt,indentation);
                 tempChildCnt++;
             }
             else if(element instanceof TerminalNodeImpl){
                 Token t = ((TerminalNodeImpl) element).getSymbol();
                 String lexerName = MiniJavaLexer.VOCABULARY.getSymbolicName(t.getType());
                 if(lexerName!=null) {
-                    //System.out.println(lexerName);
-                    //System.out.println("indentation: "+indentation);
-                    System.out.println((tempIndentation+10+tempChildCnt)+"[label=\""+lexerName+"\n"+element.getText()+"\"]");
-                    if(preIndentation.equals(""))
-                        System.out.println("00->"+(tempIndentation+10+tempChildCnt));
-                    else
-                        System.out.println(preIndentation+"->"+(tempIndentation+10+tempChildCnt));
-                    //Sequence.add(Integer.toString(indentation+1));
-                    //Type.add(lexerName);
-                    //Text.add(element.getText());
+                    System.out.println(indentation+tempChildCnt+"[label=\""+lexerName+"\n"+element.getText()+"\"]");
+                    System.out.println(indentation+"->"+(indentation+tempChildCnt));
                     tempChildCnt++;
                 }
             }
-        }
-    }
-
-    private static void toDot(){
-        printLabel();
-        int pos=0;
-        for (int i=1; i<Sequence.size();i++){
-            pos = Integer.parseInt(Sequence.get(i))-1;
-            System.out.println(pos+Integer.toString(pos)+"->"+Sequence.get(i)+i);
-        }
-    }
-
-    private static void printLabel(){
-        for(int i=0;i<Sequence.size();i++){
-            System.out.println(Sequence.get(i)+i+"[label=\""+Type.get(i));
-            System.out.println("\\n "+Text.get(i));
-            System.out.println("\"]");
         }
     }
 
