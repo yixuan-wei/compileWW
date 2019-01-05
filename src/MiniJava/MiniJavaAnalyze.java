71d98c19fb7fa9188074323e8c852c281f0cf98e
WeiyueWang <15307130349@fudan.edu.cn>
2019-01-05 12:34:07 +0800

    TreeReviseFinish

--- src/MiniJava/MiniJavaAnalyze.java
+++ src/MiniJava/MiniJavaAnalyze.java
@@ -11,12 +11,24 @@ import java.util.ArrayList;
 public class MiniJavaAnalyze {
 
     public static void main(String[] args) throws Exception{
+        //Choose to display verbose or not
+        System.out.println("please input your verbose choice: 1 for true, 0 for false");
+        int b=System.in.read();
+        while (b!=49&&b!=48) {
+            System.out.println("please input your verbose choice: 1 for true, 0 for false");
+            b = System.in.read();
+        }
+        boolean verbose = false;
+        if(b==49) verbose=true;
+        System.out.println("verbose is set to "+verbose);
+        //Input Source File in minijava format
         String inputFile = null;
         if ( args.length>0 ) inputFile = args[0];
         InputStream is = System.in;
         if ( inputFile!=null ) {
             is = new FileInputStream(inputFile);
         }
+        //Construct ANTLR analyze tools
         ANTLRInputStream input = new ANTLRInputStream(is);
         MiniJavaLexer lexer = new MiniJavaLexer(input);
         CommonTokenStream tokens = new CommonTokenStream(lexer);
@@ -33,19 +45,18 @@ public class MiniJavaAnalyze {
             out=new PrintStream("src/draw/test.dot");
             System.setOut(out);
             System.out.println("digraph AST{");
-            //toDot();
-            generateAST(ctx,true,"0","");
+            generateAST(ctx,verbose,"0","");
             System.out.println("}");
         } catch (FileNotFoundException e){
             e.printStackTrace();
         }
+        //proceed graphviz command through Process
         System.setOut(System.out);
         String path = "/Users/weiyixuan/Desktop/复旦/课程/大四上/编译/project/src/";
-        //path = "";
         String command = path+"draw/dot -Tpng -o "+path+"graph.png "+path+"draw/test.dot";
         String openCmd = "open "+path+"graph.png";
         Process p = Runtime.getRuntime().exec(command);
-
+        //Handle errors and output
         InputStream is2 = p.getInputStream();
         InputStream errorIS = p.getErrorStream();
         BufferedReader reader = new BufferedReader(new InputStreamReader(is2));
@@ -69,11 +80,13 @@ public class MiniJavaAnalyze {
     }
 
     private static void generateAST(RuleContext ctx, boolean verbose, String indentation, String preIndentation) {
-        boolean ignored = !verbose && ctx.getChildCount() == 1 && ctx.getChild(0) instanceof ParserRuleContext;
+        boolean ignored = ctx.getChildCount() == 1 && ctx.getChild(0) instanceof ParserRuleContext;
         if (!ignored) {
             String ruleName = MiniJavaParser.ruleNames[ctx.getRuleIndex()];
-            System.out.println(indentation+"[label=\""+ruleName+"\"]");
-            //System.out.println(indentation+"[label=\""+ruleName+"\n"+ctx.getText()+"\"]");
+            if(verbose)
+                System.out.println(indentation+"[label=\""+ruleName+"\n"+ctx.getText()+"\"]");
+            else
+                System.out.println(indentation+"[label=\""+ruleName+"\"]");
             if(!preIndentation.equals("")) {
                 System.out.println(preIndentation+"->"+indentation);
             }
